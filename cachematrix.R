a0f7a773c2ae3568d8d46da06bfbdf14afc5a215
slascala <slascala@users.noreply.github.com>
2014-04-22 20:16:39 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+### This function will calculate and store the inverse of a matrix in a cache matrix 
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
+ 
 
+ 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
+## it retruns the cached matrix if it exists otherwise it calculates the inverse and stores
+## it via the cache matrix object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+ 
+
+ 
